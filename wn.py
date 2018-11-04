4969b25978183fdb3a79c1e36bbbcfa75048d5f1
McKenzie <mac.gray.311@gmail.com>
2018-11-03 22:07:32 -0700

    switched file names; this way makes more sense

--- wn.py
+++ wn.py
@@ -1,73 +1,108 @@
 from sys import argv
 import string
 
-ones_words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eightteen", "nineteen"]
-tens_words = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-thousands_words = ["thousand", "million", "billion", "trillion", "quadrillion"]
+# Max int = 9,223,372,036,854,775,807 
+number_words = {
+    "zero":         0,
+    "one":          1,
+    "two":          2,
+    "three":        3,
+    "four":         4,
+    "five":         5,
+    "six":          6,
+    "seven":        7,
+    "eight":        8,
+    "nine":         9,
+    "ten":          10,
+    "eleven":       11,
+    "twelve":       12,
+    "thirteen":     13,
+    "fourteen":     14,
+    "fifteen":      15,
+    "sixteen":      16,
+    "seventeen":    17,
+    "eightteen":    18,
+    "nineteen":     19,
+    "twenty":       20,
+    "thirty":       30,
+    "forty":        40,
+    "fifty":        50,
+    "sixty":        60,
+    "seventy":      70,
+    "eighty":       80,
+    "ninety":       90,
+    "hundred":      100,
+    "thousand":     1000,
+    "million":      1000000,
+    "billion":      1000000000,
+    "trillion":     1000000000000,
+    "quadrillion":  1000000000000000
+}
 
+thousand_powers = [x for x in number_words.keys() if number_words[x] >= 1000]
+
+commas = False
+debug = False
 sign = False
 
 # Set flags according to command line arguments
 for a in argv[1:]:
     if a[0] == '-' and len(a) > 1:
         for c in a[1:]:
-            if c == 's':
+            if c == 'c':
+                commas = True
+            elif c == 'd':
+                debug = True
+            elif c == 's':
                 sign = True
 
 print("Enter 'exit' to quit")
 
 while (True):
-    input_str = input("Number:").replace(',','') # Input will always be a string
+    negative = False
+
+    input_str = input("Words:")
 
     if input_str in ['quit', 'exit']:
         quit()
 
-    negative = False
-    if input_str[0] in ['+', '-']:
-        if input_str[0] == '-':
-            negative = True
-        input_str = input_str[1:]
+    input_list = input_str.replace('-',' ').replace(',','').lower().split()
 
-    if not input_str.isdigit():
-        print("Error: input must be a positive integer")
-        continue
-    if len(input_str) > 15:
-        print("Error: input cannot be larger than 15 digits")
-        continue
+    if input_list[0] == "negative":
+        negative = True
 
-    i = 0 # The digit position in the reversed number
-    b = 0 # The position in the current block
-    last_digit = 0
-    output = []
-    for c in reversed(input_str): # reversed() iterates through the string backward without creating a copy of the string
-        if b == 0 and i > 0:
-            output.append(thousands_words[i//3 - 1]) # The // operator performs integer division
+    input_list = [x for x in input_list if x in number_words.keys()]
 
-        digit = int(c)
-        if b == 0 and (digit != 0 or len(input_str) == 1):
-            output.append(ones_words[digit])
-        elif b == 1 and digit != 0:
-            if digit != 1:
-                # Tens digit is at least 2
-                output.append(tens_words[digit - 2])
+    if debug:
+        print(input_list)
+        print("Commas: " + str(commas))
+
+    total = 0
+    block = 0
+
+    for i in range(len(input_list)):
+        if input_list[i] in thousand_powers:
+            # End the block
+            total += block * number_words[input_list[i]]
+            block = 0
+        else:
+            # Build the block
+            if input_list[i] == "hundred":
+                block *= 100
             else:
-                # Tens digit is a 1
-                if last_digit != 0:
-                    output[-1] = ones_words[last_digit + 10]
-                else:
-                    output.append(ones_words[last_digit + 10])
-        elif b == 2 and digit != 0:
-            output.append("hundred")
-            output.append(ones_words[digit])
+                block += number_words[input_list[i]]
+                
+        if i == len(input_list) - 1:
+            # End of words
+            total += block
 
-        i = i + 1
-        b = i % 3
-        last_digit = digit
+    output = total
+    if commas:
+        output = "{:,}".format(total)
 
     if negative:
-        output.append("negative")
+        output = '-' + str(output)
     elif sign:
-        output.append("positive")
-
-    print(" ".join(reversed(output)))
+        output = '+' + str(output)
+        
+    print(output)
